7c1886ef8c4e78e092969cbdb0164fa93cda25b0
One_Piece <1454448188@qq.com>
2024-10-09 01:00:40 +0800

    02class freertos print helloWorld

---
+++ 02class_freertos_print_helloworld/APP/Key/Inc/Key.h
@@ -0,0 +1,9 @@
+#ifndef __KEY_H__
+#define __KEY_H__
+
+#include "gpio.h"
+
+#define KEY_USER_STATUS HAL_GPIO_ReadPin(KEY_USER_GPIO_Port,KEY_USER_Pin)
+
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# RTOS_CLASS
+# 01class 按键控制在灯程序 在分支LED_Initial
+# 02clase rtos输出helloworld 在主分支main
